6caefd80eeeb5a0f0dde7f4ab39769ecbafaa199
hajjm200 <166666792+hajjm200@users.noreply.github.com>
2025-06-26 15:28:59 -0700

    Create hajjm_assignment1.c

---
+++ hajjm_assignment1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159265359
+
+int main(){
+  int n, i = 0;
+    float R, ha, hb;
+    float lateral, top, bottom, total_surface, volume;
+    float sum_surface = 0.0, sum_volume = 0.0;
+
+  do {
+        printf("How many spherical segments you want to evaluate [2-10]? ");
+        scanf("%d", &n);
+    } while (n < 2 || n > 10);
+
+  
+}
